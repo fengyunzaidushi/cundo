ab386e95fb71d59a2633ad20b861280587936df8
fengyunzaidushi <fengxiaoyang163@163.com>
2025-08-14 21:09:02 +0800

    Fix Jest configuration for ES modules

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,7 @@ export default {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
   },
-  testMatch: ['**/test/**/*.test.js']
+  testMatch: ['**/test/**/*.test.js'],
+  extensionsToTreatAsEsm: ['.js'],
+  moduleFileExtensions: ['js', 'json', 'node']
 };
